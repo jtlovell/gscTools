541cea30e4e21f0bdabf85bdf9ac9dd99491b3c0
jtlovell <johntlovell@gmail.com>
2023-03-05 13:26:41 -0700

    added map_contigsTelos

--- Rscripts/map_contigsTelos.R
+++ Rscripts/map_contigsTelos.R
@@ -72,7 +72,7 @@ add_nogapBlocks <- function(chrLens, kmerBed){
   tpe <- tp[,list(start = ifelse(all(is.na(end)), 1L, as.integer(max(end, na.rm = T)+1)), 
                   end = chrEnd[1]), by = "chr"]
   tpe[,type := "noGap"]
-
+  
   tp <- rbind(tpo, tpe)
   setkey(tp, chr, start, end)
   tp[,width := end - start]
@@ -100,7 +100,37 @@ get_teloPos <- function(bed, chrLens, teloBuff){
   return(bedMd)
 }
 
-classify_engine <- function(faFile, teloKmers, gapSize, teloGapSize, minTeloSize, teloRepDens, binScaffolds, verbose){
+rename_chrs <- function(ss, splitNames){
+  ns <- as.data.table(tstrsplit(trimws(names(ss)), " "))
+  if(ncol(ns) == 1){
+    # -- if one column (no whitespace) return names
+    chrns <- ns[[1]]
+  }else{
+    # -- if there is a column with "chr" in it and it is all unique use that
+    wh <- grepl("chr", tolower(unlist(ns[1,])))
+    us <- apply(ns, 2, function(x) all(!duplicated(x)))
+    if(sum(wh & us) == 1){
+      chrns <- as.character(ns[[wh & us]])
+    }else{
+      # -- if there is a column with "chr", followed by one totally unique, use that
+      if(sum(wh) == 1){
+        index <- which(wh) + 1
+        if(all(!duplicated(ns[[index]]))){
+          chrns <- ns[[index]]
+        }else{
+          # -- otherwise just return the names
+          chrns <- names(ss)
+        }
+      }else{
+        chrns <- names(ss)
+      }
+    }
+  }
+  names(ss) <- chrns
+  return(ss)
+}
+
+classify_engine <- function(faFile, teloKmers, gapSize, teloGapSize, minTeloSize, teloRepDens, binScaffolds, verbose, splitNames, minChrSize){
   
   if(!requireNamespace("Biostrings", quietly = TRUE))
     stop("you need to install Biostrings from Bioconductor before running\n")
@@ -113,37 +143,71 @@ classify_engine <- function(faFile, teloKmers, gapSize, teloGapSize, minTeloSize
   
   # -- read in the fasta as a DNAStringSet
   ss <- readDNAStringSet(faFile)
+  ss <- ss[width(ss) > minChrSize]
+  ss <- rename_chrs(ss)
+  
   chrLens <- pull_chrLens(ss)
+  chrLens <- subset(chrLens, complete.cases(chrLens))
+  chrLens[,`:=`(v1 = frank(chr), v2 = frank(as.numeric(gsub("[^0-9.-]", "", chr))))]
+  setkey(chrLens, v2, v1, chr)
+  chrLens[,`:=`(v1 = NULL, v2 = NULL)]
+  
   contigN50 <- N50(chrLens$chrEnd)
   if(verbose)
-    cat(sprintf("\tN. scaffolds = %s (N50 = %sMb); total length = %sMb\n",
-                nrow(chrLens), 
-                round(N50(chrLens$chrEnd) / 1e6, 2), 
-                round(N50(sum(chrLens$chrEnd)) / 1e6, 2)))
+    cat(strwrap(sprintf(
+      "\tN. scaffolds = %s (N50 = %sMb); total length = %sMb\n",
+      nrow(chrLens), 
+      round(N50(chrLens$chrEnd) / 1e6, 2), 
+      round(N50(sum(chrLens$chrEnd)) / 1e6, 2)),
+      indent = 8, exdent = 16), sep = "\n")
   
   # -- make a bed with the start and end positions of the gap kner (N)
   # then cluster into gap coordinates
   gaps <- find_kmerPos(dnass = ss, kmer = "N")
-  gapsClus <- cluster_kmerPos(
-    bed = gaps, minGap = gapSize, minDens = 1, minWidth = gapSize)
+  if(!is.null(gaps)){
+    if(nrow(gaps) > 0){
+      gapsClus <- cluster_kmerPos(
+        bed = gaps, minGap = 1, minDens = 1, minWidth = 1)
+      gapsClus[,type := "gap"]
+    }else{
+      gapsClus <- NULL
+    }
+  }else{
+    gapsClus <- NULL
+  }
   
   # -- make a bed with the start and end positions of the telomere kmers
   # then cluster into gap coordinates
-  teloKmers <- c(teloKmers, 
-                 as.character(reverseComplement(DNAStringSet(teloKmers))))
+  teloKmers <- unique(c(teloKmers, 
+                        as.character(reverseComplement(DNAStringSet(teloKmers)))))
   telos <- rbindlist(lapply(teloKmers, function(i)
     find_kmerPos(dnass = ss, kmer = i)))
-  telos <- subset(telos, !duplicated(telos))
-  telosClus <- cluster_kmerPos(
-    bed = telos, 
-    minGap = teloGapSize,
-    minDens = teloRepDens, 
-    minWidth = minTeloSize)
+  if(!is.null(telos)){
+    telos <- subset(telos, !duplicated(telos))
+    telosClus <- cluster_kmerPos(
+      bed = telos, 
+      minGap = teloGapSize,
+      minDens = teloRepDens, 
+      minWidth = minTeloSize)
+    telosClus[,type := "telomere"]
+  }else{
+    telosClus <- NULL
+  }
   
   # -- compile data
-  telosClus[,type := "telomere"]
-  gapsClus[,type := "gap"]
-  kmerClus <- rbind(telosClus, gapsClus)
+  
+  if(is.null(gapsClus) && is.null(telosClus))
+    stop("could not find any telomeres or gaps ... reduce search criteria stringency\n")
+  
+  if(is.null(telosClus)){
+    kmerClus <- data.table(gapsClus)
+  }else{
+    if(is.null(gapsClus)){
+      kmerClus <- data.table(telosClus)
+    }else{
+      kmerClus <- rbind(telosClus, gapsClus)
+    }
+  }
   
   # -- add the no-gap, no-telomere blocks
   outBed <- add_nogapBlocks(
@@ -151,9 +215,16 @@ classify_engine <- function(faFile, teloKmers, gapSize, teloGapSize, minTeloSize
     kmerBed = kmerClus)
   
   # -- compile contig metadata
-  contigBed <- add_nogapBlocks(
-    chrLens = chrLens, 
-    kmerBed = gapsClus)
+  if(is.null(gapsClus)){
+    contigBed <- with(chrLens, data.table(
+      chr = chr, chrStart = chrStart, chrEnd = chrEnd, 
+      start = chrStart, end = chrEnd, width = chrEnd, type = "noGap", index = 1))
+  }else{
+    contigBed <- add_nogapBlocks(
+      chrLens = chrLens, 
+      kmerBed = gapsClus)
+  }
+  
   if(binScaffolds)
     contigBed$chr[grepl("scaff", contigBed$chr)] <- "scaffolds"
   
@@ -161,20 +232,24 @@ classify_engine <- function(faFile, teloKmers, gapSize, teloGapSize, minTeloSize
   if(binScaffolds){
     contigs <- subset(contigs, chr != "scaffolds")
     if(verbose)
-      cat(sprintf("\t%s contigs exl. scaffs (N50 = %s, total = %sMb); %s gaps (%sMb)\n",
-                  nrow(contigs), 
-                  round(N50(contigs$width) / 1e6, 2), 
-                  round(N50(sum(contigs$width)) / 1e6, 2),
-                  sum(outBed$type == "gap"),
-                  round(sum(outBed$width[outBed$type == "gap"])/1e6, 2)))
+      cat(strwrap(sprintf(
+        "\t%s contigs exl. scaffs (N50 = %sMb, total = %sMb); %s gaps (%sMb)\n",
+        nrow(contigs), 
+        round(N50(contigs$width) / 1e6, 2), 
+        round(N50(sum(contigs$width)) / 1e6, 2),
+        sum(outBed$type == "gap"),
+        round(sum(outBed$width[outBed$type == "gap"])/1e6, 2)),
+        indent = 8, exdent = 16), sep = "\n")
   }else{
     if(verbose)
-      cat(sprintf("\t%s contigs incl. scaffs (N50 = %s, total = %sMb); %s gaps (%sMb)\n",
-                  nrow(contigs), 
-                  round(N50(contigs$width)/1e6, 2), 
-                  round(N50(sum(contigs$width))/1e6, 2),
-                  sum(outBed$type == "gap"),
-                  round(sum(outBed$width[outBed$type == "gap"])/1e6, 2)))
+      cat(strwrap(sprintf(
+        "%s contigs incl. scaffs (N50 = %sMb, total = %sMb); %s gaps (%sMb)\n",
+        nrow(contigs), 
+        round(N50(contigs$width)/1e6, 2), 
+        round(N50(sum(contigs$width))/1e6, 2),
+        sum(outBed$type == "gap"),
+        round(sum(outBed$width[outBed$type == "gap"])/1e6, 2)),
+        indent = 8, exdent = 16), sep = "\n")
   }
   
   # -- compile telomere metadata
@@ -187,6 +262,16 @@ classify_engine <- function(faFile, teloKmers, gapSize, teloGapSize, minTeloSize
                   grepl("left", teloMd$teloPosition)))
   nTeloChrs <- sum(grepl("right|left", teloMd$teloPosition))
   
+  tmp <- subset(teloMd, teloPosition != "none")
+  tmp[,prnt := ifelse(grepl("left", teloPosition) & grepl("right", teloPosition), sprintf("%sLR", chr, chr),
+                      ifelse(grepl("left", teloPosition), sprintf("%sL", chr), sprintf("%sR", chr)))]
+  tmp[,chr := factor(chr, levels = chrLens$chr)]
+  setkey(tmp, chr)
+  if(verbose)
+    cat(strwrap(sprintf(
+      "\tChrs w/ telos: %s", paste(tmp$prnt, collapse = " ")),
+      indent = 8, exdent = 16), sep = "\n")
+
   # -- compile full metadata
   
   contigMd <- contigBed[,list(
@@ -240,7 +325,7 @@ plot_classes <- function(blockCalls, minChrSize2plot = 1e6, palette = viridisLit
       type, chr, FUN = function(x) sum(x == "noGap"))))
     nColors <- ifelse(mxn/3 < 5, 5, mxn / 3)
   }
-    
+  
   cols <- palette(nColors, ...)
   
   tp <- subset(blockCalls, chr != "scaffolds")
@@ -285,17 +370,19 @@ plot_classes <- function(blockCalls, minChrSize2plot = 1e6, palette = viridisLit
   print(p)
 }
 
-classify_genomes <- function(faFiles, toStrip = ".fa$|.fa.gz$", teloKmers = c("CCCGAAA", "CCCTAAA"), gapSize = 1e4, teloGapSize = 1e4, minTeloSize = 1e3, teloRepDens = 0.1, binScaffolds = TRUE, minChrSize2plot = 1e6, palette = viridisLite::viridis, nColors = 25, barThickness = .6, backgroundColor = "darkgrey", verbose = TRUE, ...){
+classify_genomes <- function(faFiles, toStrip = ".fa$|.fa.gz$", teloKmers = c("CCCGAAA", "CCCTAAA"), gapSize = 1e4, teloGapSize = 1e4, minTeloSize = 1e3, teloRepDens = 0.1, binScaffolds = TRUE, minChrSize2plot = 1e6, palette = viridisLite::viridis, nColors = 25, barThickness = .6, backgroundColor = "darkgrey", verbose = TRUE, splitNames = TRUE, ...){
   
   classes <- lapply(faFiles, function(i){
     cat(i,"\n")
     test <- classify_engine(
       faFile = i, 
+      splitNames = splitNames, 
       teloKmers = teloKmers,
       gapSize = gapSize,
       teloGapSize = teloGapSize,
       minTeloSize = minTeloSize,
-      teloRepDens = teloRepDens, 
+      teloRepDens = teloRepDens,
+      minChrSize = minChrSize2plot, 
       binScaffolds = binScaffolds, 
       verbose = verbose)
     test$metadata[,genome := gsub(toStrip, "", basename(i))]
@@ -339,19 +426,17 @@ parser$add_argument(
   "-t", "--telokmers", type = "character", default = "CCCGAAA,CCCTAAA", 
   help = "comma-separated string of paths to fasta files")
 parser$add_argument(
-  "-z", "--teloclust", type = "character", default = "1e4,1e3,0.1", 
+  "-z", "--teloclust", type = "character", default = "250,500,0.5", 
   help = "comma-separated cluster params (max gap, min width, min %telo sequence) [default = %(default)s]")
 parser$add_argument(
   "-b", "--binScaffolds", action="store_false", dest = "binScaffolds", 
   help = "Should any chrs with scaff in their name be binned [default = %(default)s]")
-
 parser$add_argument(
   "-p", "--pdfFile", type = "character", default = "contigPlot.pdf",
   help = "output pdf file name [default= %(default)s]")
 parser$add_argument(
   "-o", "--outFile", type = "character", default = "contigCoords.txt",
   help = "output text file with contig coordinates [default= %(default)s]")
-
 parser$add_argument(
   "-m", "--minChrLen2plot", type = "integer", default = 1e6,
   help = "the smallest chromosome to show [default= %(default)s]")
@@ -359,19 +444,14 @@ parser$add_argument(
   "-g", "--gapSize", type = "integer", default = 1e4,
   help = "window size to average over to merge gaps [default= %(default)s]")
 parser$add_argument(
-  "-d", "--height", type = "double", default = 3,
-  help = "numeric, the height of the plot [default= %(default)s]")
-parser$add_argument(
-  "-w", "--width", type = "double", default = 3,
-  help = "numeric, the width of the plot / genome [default= %(default)s]")
-parser$add_argument(
   "-r", "--regex", type = "character", default = ".fa.gz$|.fa$",
   help = "quoted character, a regex to strip from the file name [default= %(default)s]")
-
 parser$add_argument(
   "-q", "--quietly", action = "store_false", 
   dest = "verbose", help = "Print little output")
-
+parser$add_argument(
+  "-n", "--splitNames", action = "store_false", 
+  dest = "splitNames", help = "Split chromosome names on the first whitespace")
 parser$add_argument(
   "-c", "--print2stdout", action = "store_true", 
   dest = "print2stdout", help = "print the summary file to stdout instead of outfile")
@@ -428,10 +508,11 @@ barThicknessIn <- .6
 backgroundColorIn <- "darkgrey"
 verboseIn <- args$verbose
 pdfFileIn <- args$pdfFile
+splitNamesIn <- args$splitNames
 
 # -- write the plot to file
-plotx <- floor(sqrt(length(faFilesIn))) * 4 + .5
-ploty <- ceiling(sqrt(length(faFilesIn))) * 3
+plotx <- floor(sqrt(length(faFilesIn))) * 4 + 1.5
+ploty <- ceiling(sqrt(length(faFilesIn))) * 3 + 1
 pdf(pdfFileIn, height = ploty, width = plotx)
 
 test <- classify_genomes(
@@ -448,6 +529,7 @@ test <- classify_genomes(
   nColors = nColorsIn, 
   barThickness = barThicknessIn, 
   backgroundColor = backgroundColorIn, 
+  splitNames = splitNamesIn, 
   verbose = verboseIn)
 
 nope <- dev.off()
